1eea51ace83f7ef4d2106446e29a75aad748daad
AlbertoVf <AlbertoVf97@protonmail.com>
2021-02-05 16:43:20 +0100

    Diseño y posicionamiento de la ventana principal

---
+++ src/Interfaz/Frame.java
@@ -0,0 +1,23 @@
+package Interfaz;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Frame extends JFrame {
+    Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+    int x = (int) ((dimension.getWidth() - this.getWidth()) / 2);
+    int y = (int) ((dimension.getHeight() - this.getHeight()) / 2);
+
+    public Frame(String tipo) {
+        if (tipo.equals("Codificar")) {
+            setBounds(x / 2, y / 2, x / 2 + 50, x / 2 + 130);
+            add(new Codificar());
+        } else if (tipo.equals("Decodificar")) {
+            setBounds(x / 2, y / 2, x, y);
+            add(new Decodificar());
+        }
+        setVisible(true);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
