d3cb630259a51145e0ac91665e02b3bbe14e9c2d
AlbertoVf <AlbertoVf97@protonmail.com>
2021-02-05 16:48:47 +0100

    Creacion de test para QR

---
+++ test/QRTest.java
@@ -0,0 +1,66 @@
+import QR.QR;
+import com.google.zxing.ChecksumException;
+import com.google.zxing.FormatException;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.WriterException;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+public class QRTest {
+    QR q = new QR();
+
+    @Test
+    public void setHeight() {
+        q.setHeight(40);
+        assertEquals(40, q.getHeight());
+    }
+
+    @Test
+    public void setWidth() {
+        q.setWidth(40);
+        assertEquals(40, q.getWidth());
+    }
+
+    @Test
+    public void setName_file() {
+        q.setNameQr("codigo-qr");
+        assertEquals("codigo-qr", q.getNameQr());
+
+        q.setNameQr("qr-code!ªº{}[]?¿¡'\"@");
+        assertEquals("qr-code_____________", q.getNameQr());
+    }
+
+    @Test
+    public void generarImagen() {
+        File f = new File("qr-prueba.png");
+        try {
+            assertEquals(f, q.generarImagen());
+        } catch (IOException | WriterException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void generateQR() {
+        try {
+            assertEquals(new File("qr-prueba.png"), q.generateQR(new File("qr-prueba.png"), "Texto del codigo QR", 50, 50));
+        } catch (WriterException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void decoder() {
+        String a = null;
+        try {
+            a = q.decoder(new File("qr-prueba.png"));
+        } catch (IOException | FormatException | ChecksumException | NotFoundException e) {
+            e.printStackTrace();
+        }
+        assertEquals("Texto del codigo QR", a);
+    }
+}
